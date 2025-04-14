087d3e0bf7da54e4357bc680b89386d940a5678d
lammieow <cedric@Bandcamps-MacBook-Pro.local>
2025-04-14 10:00:52 -0400

    initial code for job table and job form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import JobTable from './components/JobTable.jsx';
+import '@ant-design/v5-patch-for-react-19';
+
 
 function App() {
   return (
     <>
       <h1>Manifestify</h1>
+      <>
+      <JobTable/>
+      </>
     </>
   );
 }
