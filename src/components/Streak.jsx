a94ede819586ecdfa7c95e7071795e617d8e0b16
chriskhoa <nkhoa@umich.edu>
2025-04-20 14:09:22 -0400

    Update streak logic to calculate the list of submitted application

--- src/components/Streak.jsx
+++ src/components/Streak.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import streakMessages from "../util/streakMessages";
 
-const Streak = () => {
-  const [streak, setStreak] = useState(0);
+const Streak = ({ streak }) => {
+  // const [streak, setStreak] = useState(0);
   return (
     <div>
       <h2>{streak}</h2>
@@ -16,7 +16,7 @@ const Streak = () => {
           ? streakMessages.find((item) => item.id === streak).emoji
           : "🌟"}
       </p>
-      <button onClick={() => setStreak((prev) => prev + 1)}>Test</button>
+      {/* <button onClick={() => setStreak((prev) => prev + 1)}>Test</button> */}
     </div>
   );
 };
